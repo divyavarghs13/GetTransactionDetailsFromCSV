255676b00a843f16771d9a01c3d1eac379b5801f
Divya <Divya@192.168.0.126>
2019-03-12 04:43:17 +0530

    second commit

--- mytask/src/main/resources/static/result/result.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/welcome"
-    }).then(function(data, status, jqxhr) {
-       $('.transaction-id').append(data.id);
-       $('.transaction-date').append(data.content);
-       console.log(jqxhr);
-    });
-});
